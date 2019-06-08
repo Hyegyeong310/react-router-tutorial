39e78e3c7869913ad4aa9476a00ace15a1851932
Hyegyeong310 <hyegyeong310@gmail.com>
2019-06-08 23:23:25 +0900

    modified route path

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const About = () => {
+const About = ({ match }) => {
   return (
     <div>
       <h2>소개</h2>
-      <p>안녕하세요, 저는 리액트 라우터입니다.</p>
+      <p>안녕하세요, 저는 {match.params.name}입니다.</p>
     </div>
   );
 };
